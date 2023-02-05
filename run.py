c8b39ea6b5fc82860e4faf59b46ee3efba849a60
Jeet <kumarzeet19@gmail.com>
2023-02-06 01:13:36 +0530

    feat: Add sign-up, login, logout, and notes functionality
    - Implement sign-up and login pages with authentication.
    - Add the ability for logged-in users to create, view, and manage notes.
    - Implement logout functionality for logged-in users.

--- run.py
+++ run.py
@@ -1,2 +1,3 @@
-from app import app
+from app import create_app
+app = create_app()
 app.run(debug=True, host='0.0.0.0', port=80)
---
+++ static/index.js
@@ -0,0 +1,9 @@
+function deleteNote(noteId) {
+    fetch("/delete-note", {
+      method: "POST",
+      body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+  }
+  
